12e2815df7e580787db7dcdab33330fa0f21a673
hyewon <gpdnjs154@naver.com>
2024-04-26 02:38:34 +0900

    [update]폼데이터 auth/register로 전달

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -2,10 +2,10 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   try {
-    const { email, password } = await request.json();
+    const { email, password ,phoneNumber, username, role, passwordCheck } = await request.json();
     // 여기에 유효성 검사를 추가하면 좋을 수 있어요
 
-    console.log({ email, password });
+    console.log({ email, password ,phoneNumber, username, role, passwordCheck});
   } catch (e) {
     console.log({ e });
   }
