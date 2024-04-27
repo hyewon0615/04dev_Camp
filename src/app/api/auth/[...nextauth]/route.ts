0468a85ac5a003b1975f607e4bc71466bf01fc92
hyewon <gpdnjs154@naver.com>
2024-04-27 18:11:33 +0900

    [update]nextauth secret 설정

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -14,7 +14,6 @@ const handler = NextAuth({
 
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
       name: "Credentials",
       credentials: {
         email: {},
